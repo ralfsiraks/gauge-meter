83c3ed80db4614b60b9f75e046c39babf974bbab
ralfsiraks <hyperblazexd@gmail.com>
2023-03-22 00:07:23 +0200

    init commit

---
+++ script.js
@@ -0,0 +1,96 @@
+const process_txt = document.querySelector(".process");
+const success_txt = document.querySelector(".success");
+const bar = document.querySelector(".bar");
+const needle = document.querySelector(".needle");
+const amount = document.querySelector(".amount");
+const date = document.querySelector(".date");
+const time = document.querySelector(".time");
+
+setTimeout(() => {
+  process_txt.classList.add("active");
+}, 0);
+
+setTimeout(toggleMsg, 1600);
+
+function toggleMsg() {
+  process_txt.classList.remove("active");
+  success_txt.classList.add("active");
+}
+
+const placeholderData = {
+  day1: {
+    date: `2023/01/01`,
+    time: `12:50:00`,
+    amount: Math.floor(Math.random() * 1000),
+  },
+  day2: {
+    date: `2023/01/02`,
+    time: `12:50:00`,
+    amount: Math.floor(Math.random() * 1000),
+  },
+  day3: {
+    date: `2023/01/03`,
+    time: `12:50:00`,
+    amount: Math.floor(Math.random() * 1000),
+  },
+  day4: {
+    date: `2023/01/04`,
+    time: `12:50:00`,
+    amount: Math.floor(Math.random() * 1000),
+  },
+  day5: {
+    date: `2023/01/05`,
+    time: `12:50:00`,
+    amount: Math.floor(Math.random() * 1000),
+  },
+  day6: {
+    date: `2023/01/06`,
+    time: `12:50:00`,
+    amount: Math.floor(Math.random() * 1000),
+  },
+  day7: {
+    date: `2023/01/07`,
+    time: `12:50:00`,
+    amount: Math.floor(Math.random() * 1000),
+  },
+  day8: {
+    date: `2023/01/08`,
+    time: `12:50:00`,
+    amount: Math.floor(Math.random() * 1000),
+  },
+  day9: {
+    date: `2023/01/09`,
+    time: `12:50:00`,
+    amount: Math.floor(Math.random() * 1000),
+  },
+  day10: {
+    date: `2023/01/10`,
+    time: `12:50:00`,
+    amount: Math.floor(Math.random() * 1000),
+  },
+};
+
+const json = JSON.stringify(placeholderData);
+console.log(json);
+
+const data = JSON.parse(json);
+const entries = Object.entries(data);
+
+let counter = 0;
+
+const program = (data) => {
+  const interval = setInterval(() => {
+    const currentDay = data[counter][1];
+    amount.textContent = `${currentDay.amount}µm`;
+    date.textContent = `${currentDay.date}`;
+    time.textContent = `${currentDay.time}`;
+    const rotation = (currentDay.amount / 10) * 1.8 - 90;
+    bar.style.transform = `rotate(${rotation}deg)`;
+    needle.style.transform = `rotate(${rotation}deg)`;
+    counter++;
+
+    if (counter === data.length) clearInterval(interval);
+  }, 1000);
+};
+
+program(entries);
